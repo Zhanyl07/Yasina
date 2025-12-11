6215faacce2f9c55a58b55b3a02648a7a503044f
Zhanyl <asanakunovajanyla@gmail.com>
2025-12-12 02:22:43 +0500

    finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
- import { Link } from 'react-router-dom'
-  import "./Header.scss"
+import { Link } from 'react-router-dom'
+import "./Header.scss"
 
-function Header() { return ( <header className='header'> <div className="top-header"> <div className="header-content container"> {/* Logo */} <Link className='logo' to="/">YASINA</Link>
+function Header() {
+  return (
+    <header className='header'>
+      <div className="top-header">
+        <div className="header-content container">
 
-{/* Navigation */}
-      <ul className='nav-list'>
-        <li><Link className='nav-link' to="/works">Works</Link></li>
-        <li><Link className='nav-link' to="/blog">About</Link></li>
-        <li><Link className='nav-link' to="/contact">Contact</Link></li>
-      </ul>
+          {/* Logo */}
+          <Link className='logo' to="/">YASINA</Link>
 
-      {/* Language Switch */}
-      <select className='lang-select'>
-        <option value="kg">KGZ</option>
-        <option value="en">ENG</option>
-        <option value="ru">RUS</option>
-      </select>
-    </div>
-  </div>
-</header>
+          {/* Navigation */}
+          <ul className='nav-list'>
+            <li><Link className='nav-link' to="/works">Works</Link></li>
+            <li><Link className='nav-link' to="/blog">About</Link></li>
+            <li><Link className='nav-link' to="/contact">Contact</Link></li>
+          </ul>
 
-) }
+          {/* Language Switch */}
+          <select className='lang-select'>
+            <option value="kg">KGZ</option>
+            <option value="en">ENG</option>
+            <option value="ru">RUS</option>
+          </select>
 
-export default Header;
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,65 +2,65 @@ import React from 'react'
 import meimage from '../assets/image/me.png'
 import "./style/Home.scss"
 import Skils from './Skils';
+
 function Home() {
   return (
     <div>
-      
-            <div className="main-header container">
-              <div className="header-left">
-                <div className="header-about">
-                  <h2>
-                    Hi, I am Yasina, Front-end developer
-                  </h2>
-      
-                  <p>I build adaptive and reliable user interfaces with a strong focus on clean structure,
-attention to detail, and user-friendly experience.
-                  </p>
-      
-                  <button>Download Resume</button>
-                </div>
-              </div>
-      
-              <div className="header-right">
-                <div className="header-img">
-                  <img src={meimage} alt="" />
-                </div>
-              </div>
-            </div>
+      <div className="main-header container">
+        <div className="header-left">
+          <div className="header-about">
+            <h2>
+              Hi, I am Yasina, Front-end developer
+            </h2>
 
+            <p>
+              I build adaptive and reliable user interfaces with a strong focus on clean structure,
+              attention to detail, and user-friendly experience.
+            </p>
 
-             <div className="recent-posts container">
-  <div className="recent-header">
-    <h3>Recent posts</h3>
-    <a href="#" className="view">View all</a>
-  </div>
+            {/* 👇 Кнопка скачивания резюме */}
+            <a href="/public/resume.pdf">
+              <button className="resume-btn">Download Resume</button>
+            </a>
+          </div>
+        </div>
 
-  <div className="posts">
-    <div className="post-card">
-      <h2>Making a design system from scratch</h2>
-      <p className="info">12 Feb 2020  <p>|</p>   Design Pattern</p>
-      <p className="text">
-        Create a clean and scalable design system thet helps keep all your UI components
-        consistent.Learn how to build reusable elements and improve your workflow.
-      </p>
-    </div>
+        <div className="header-right">
+          <div className="header-img">
+            <img src={meimage} alt="Yasina" />
+          </div>
+        </div>
+      </div>
 
-    <div className="post-card">
-      <h2>Creating pixel perfect icons in Figma</h2>
-      <p className="info">12 Feb 2020  <p>|</p>  Figma, Icon Design</p>
-      <p className="text">
-        A practical guide on creating modern icons.
-        Learn spacinfg,alignment,and grid setup to make your icons sharp and
-         professional
-      </p>
-    </div>
-  </div>
-  
-</div>
+      <div className="recent-posts container">
+        <div className="recent-header">
+          <h3>Recent posts</h3>
+          <a href="#" className="view">View all</a>
+        </div>
 
-<Skils/>
+        <div className="posts">
+          <div className="post-card">
+            <h2>Making a design system from scratch</h2>
+            <p className="info">12 Feb 2020  <span>|</span>   Design Pattern</p>
+            <p className="text">
+              Create a clean and scalable design system that helps keep all your UI components
+              consistent. Learn how to build reusable elements and improve your workflow.
+            </p>
+          </div>
 
+          <div className="post-card">
+            <h2>Creating pixel perfect icons in Figma</h2>
+            <p className="info">12 Feb 2020  <span>|</span>  Figma, Icon Design</p>
+            <p className="text">
+              A practical guide on creating modern icons.
+              Learn spacing, alignment, and grid setup to make your icons sharp and
+              professional.
+            </p>
+          </div>
+        </div>
+      </div>
 
+      <Skils/>
     </div>
   );
 }
